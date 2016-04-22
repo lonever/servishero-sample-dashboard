92e51dd5caf685e7960f29dc3ca4ce877145368c
Leland Lo <lcherng@ascensionlab.com>
2016-04-22 15:52:29 +0800

    Create vertical menu for dashboard

--- modules/App.js
+++ modules/App.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import NavLink from './NavLink'
+import { Grid, Column, Menu, Item, Header} from 'react-semantify'
 
 export default React.createClass({
   render() {
     return (
       <div>
-        <h1>ServisHero dashboard</h1>
-        <ul role="nav">
-          <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/repos">Repos</NavLink></li>
-        </ul>
-        {this.props.children}
+        <Grid>
+          <Column className="three wide">
+            <Menu className="vertical relaxed sticky main-menu">
+              <Item>
+                <img src="http://4b9d3madiyh1t61o64fjrmfz.wpengine.netdna-cdn.com/wp-content/uploads/sh_logo_full.png" alt="Trusted local service providers to the rescue!" />
+              </Item>
+              <NavLink to="/" onlyActiveOnIndex>Home</NavLink>
+              <NavLink to="/about">About</NavLink>
+              <NavLink to="/repos">Repos</NavLink>
+            </Menu>
+          </Column>
+          <Column className="thirteen wide">
+            {this.props.children}
+          </Column>
+        </Grid>
       </div>
     )
   }
--- modules/NavLink.js
+++ modules/NavLink.js
@@ -1,9 +1,10 @@
 // modules/NavLink.js
 import React from 'react'
 import { Link } from 'react-router'
+import { Item} from 'react-semantify'
 
 export default React.createClass({
   render() {
-    return <Link {...this.props} activeClassName="active"/>
+    return <Link {...this.props} className="ui item" activeClassName="active"/>
   }
 })
