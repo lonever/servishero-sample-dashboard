7e1e8f55d52eda058d61640a70d71c8c4735c643
Leland Lo <lcherng@ascensionlab.com>
2016-04-22 14:30:51 +0800

    Rename header and try material button

--- modules/About.js
+++ modules/About.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button, Card, CardText } from 'react-mdl';
 
 export default React.createClass({
   render() {
-    return <div>About</div>
+    return (
+      <div>
+        <h1>About</h1>
+        <Button> WHAT </Button>
+      </div>
+    )
   }
 })
