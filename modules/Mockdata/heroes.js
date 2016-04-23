befd03b4d9fdfb2000f6ce509a50c133984f3d44
Leland Lo <lcherng@ascensionlab.com>
2016-04-23 21:58:59 +0800

    added profile pic for heroes

--- modules/Mockdata/heroes.js
+++ modules/Mockdata/heroes.js
@@ -10,6 +10,7 @@ var heroes = {
       name:"Superman Cleaning Services",
       address: "41, Jalan Flyaway, Taman FlyTogether, 46100, Petaling Jaya, Selangor",
       email: "superman@gmail.com",
+      profilePic: "http://vignette2.wikia.nocookie.net/p__/images/0/0c/Superman's_classic_pose.png/revision/latest?cb=20131218234907&path-prefix=protagonist",
       phone: "+6012344444",
       averageRating: 6,
     },
@@ -18,6 +19,7 @@ var heroes = {
       name: "Batman Fitness",
       address: "Batcave",
       email: "brucewayne999@hotmail.com",
+      profilePic: "https://upload.wikimedia.org/wikipedia/en/9/90/Bale_as_Batman.jpg",
       phone: "+6012222222",
       averageRating: 9
     },
@@ -25,6 +27,7 @@ var heroes = {
       services: ["moving"],
       name: "Flash Movers",
       address: "41, Jalan Cepat, Taman Cepat",
+      profilePic:"http://images.cwtv.com/images/shows/og-image/the-flash.png",
       email: "flash@yahoo.com",
       phone: "+60172999292",
       averageRating: 8
@@ -32,6 +35,7 @@ var heroes = {
     "ahchong": {
       services: ["personal-training"],
       name: "Ah Chong Personal Training",
+      profilePic: "http://cliparts.co/cliparts/8ix/rRk/8ixrRkk4T.png",
       address: "41, Jalan Tiga, Bukit Kayu Putih",
       email: "ahchong@strong.com",
       phone: "+691233212",
@@ -40,6 +44,7 @@ var heroes = {
     "lim": {
       services: ["cleaning-home"],
       name: "Lims Home Cleaning",
+      profilePic: 'http://cliparts.co/cliparts/8ix/rRk/8ixrRkk4T.png',
       address: "41, Jalan Tiga, Bukit Kayu Putih",
       email: "lim@clean.com",
       phone: "+691233212",
@@ -48,6 +53,7 @@ var heroes = {
     "karatekidz": {
       services: ["karate", "yoga"],
       name: "Karate Kidz",
+      profilePic: "http://cliparts.co/cliparts/8ix/rRk/8ixrRkk4T.png",
       address: "41, Jalan Tiga, Bukit Kayu Putih",
       email: "karate@kidz.com",
       phone: "+691233212",
@@ -55,6 +61,7 @@ var heroes = {
     },
     "aquaman": {
       services: ["swimming"],
+      profilePic: "http://static7.comicvine.com/uploads/scale_small/8/83882/2327247-aquaman2.jpg",
       name: "Aquaman Swimmers",
       address: "41, Jalan Tiga, Bukit Kayu Putih",
       email: "aquaman@live.com",
