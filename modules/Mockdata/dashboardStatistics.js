b584d8b84035a27295869c3f172c5eb82a555a8a
Leland Lo <lcherng@ascensionlab.com>
2016-04-24 00:02:10 +0800

    added dashboard mockdata

---
+++ modules/Mockdata/dashboardStatistics.js
@@ -0,0 +1,8 @@
+var statistics = {
+  heroesRecruited: 2,
+  request: 54,
+  quotes: 74,
+  fulfilled: 35
+};
+
+export default statistics
---
+++ modules/Mockdata/topWeekly.js
@@ -0,0 +1,19 @@
+var weeklyTop = {
+  "1": {
+    serviceType: "Personal Training",
+    number: 345,
+    icon: "male"
+  },
+  "2": {
+    serviceType: "Cleaning(home)",
+    number: 234,
+    icon: "recycle"
+  },
+  "3": {
+    serviceType: "Moving",
+    number: 123,
+    icon: "shipping"
+  }
+};
+
+export default weeklyTop
